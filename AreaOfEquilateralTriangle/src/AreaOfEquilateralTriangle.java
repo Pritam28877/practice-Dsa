d2e0c68c52488c18a0e27a107c2b9d7265db0144
Pritam Das <92548589+Pritam28877@users.noreply.github.com>
2022-01-23 12:52:25 +0530

    Add files via upload

---
+++ AreaOfEquilateralTriangle/src/AreaOfEquilateralTriangle.java
@@ -0,0 +1,13 @@
+package basic;
+
+import java.util.Scanner;
+
+public class AreaOfEquilateralTriangle {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter side of triangle: ");
+        float a = in.nextFloat();
+        double area = (Math.sqrt(3) * a * a)/4;
+        System.out.println("Area of equilateral triangle: "+area);
+    }
+}
