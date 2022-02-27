0f1827efa56c836640ecc92b9e9156bca8f458f4
Pritam Das <92548589+Pritam28877@users.noreply.github.com>
2022-02-27 18:19:47 +0530

    Add files via upload

---
+++ FindGCDExample6.java
@@ -0,0 +1,15 @@
+public class FindGCDExample6  
+{   
+public static void main(String[] args)   
+{   
+int a = 112, b = 543;   
+System.out.println("GCD of " + a +" and " + b + " is " + findGCD(a, b));   
+}  
+//recursive function to return gcd of a and b   
+static int findGCD(int a, int b)   
+{   
+if (b == 0)   
+return a;     
+return findGCD(b, a % b);   
+}   
+}  
