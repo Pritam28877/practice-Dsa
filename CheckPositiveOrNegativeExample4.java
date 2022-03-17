be21f832144eebe5f5c63062f6c81edd06f4e95f
Pritam Das <92548589+Pritam28877@users.noreply.github.com>
2022-03-17 23:19:35 +0530

    Add files via upload

---
+++ CheckPositiveOrNegativeExample4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;  
+import java.lang.Integer.*;  
+public class CheckPositiveOrNegativeExample4  
+{  
+public static void main(String[] args)   
+{  
+int num, result;  
+//object of the Scanner class  
+Scanner sc = new Scanner(System.in);  
+System.out.print("Enter a number you want to check: ");  
+//taking an integer value from the user  
+num = sc.nextInt();  
+//invoking signum() method of the Integer class  
+result=Integer.signum(num);  
+//prints the result  
+System.out.print(result);  
+}  
+}  
